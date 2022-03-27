01655ba1bcf2082d85f178f89e6d2f4efa63b539
Savio Resende <savio@savioresende.com.br>
2022-03-26 22:24:48 -0600

    Added password conf.

--- views/register.php
+++ views/register.php
@@ -31,6 +31,15 @@
                     </div>
                 </div>
 
+                <div>
+                    <label for="password" class="block text-sm font-medium text-gray-700">
+                        Password Confirmation
+                    </label>
+                    <div class="mt-1">
+                        <input id="password_confirmation" name="password_confirmation" type="password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+                    </div>
+                </div>
+
                 <div class="flex items-center justify-between">
                     <div class="flex items-center">
                         <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded">
